15538458509763f6465405f0d2a1a0a58f528676
Iryna Boiarska <boyarskairyna32@gmail.com>
2026-08-30 00:42:33 +0300

    style: add Header styles, fix Header and global.css

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,20 +1,32 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import css from './Header.module.css';
 import Image from 'next/image';
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
-    <header>
-      <Link href="/" aria-label="Home">
+    <header className={css.header}>
+      <Link className={css.logo} href="/" aria-label="Home">
         <Image src="/logo.svg" alt="TravelTrucks" width={136} height={16} loading="eager" />
       </Link>
-      <nav>
-        <ul>
+      <nav className={css.nav}>
+        <ul className={css.list}>
           <li>
-            <Link href="/">Home</Link>
+            <Link className={`${css.link} ${pathname === '/' ? css.active : ''}`} href="/">
+              Home
+            </Link>
           </li>
           <li>
-            <Link href="/catalog">Catalog</Link>
+            <Link
+              className={`${css.link} ${pathname === '/catalog' ? css.active : ''}`}
+              href="/catalog"
+            >
+              Catalog
+            </Link>
           </li>
         </ul>
       </nav>
@@ -23,5 +35,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// додати класи для стилізації
